46e4b0c0a17655eb4fa62ce95f4e306a931092ea
ylbafle <cathlin.abigail@ui.ac.id>
2025-11-05 14:04:08 +0700

    Tutorial 7 Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'menu.dart';
+import 'screen/menu.dart';
 
 void main() {
   runApp(const MyApp());
---
+++ lib/widgets/left_drawer.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:football_news/screen/menu.dart';
+import 'package:football_news/screen/newslist_form.dart';
+
+class LeftDrawer extends StatelessWidget {
+  const LeftDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),child: Column(
+              children: [
+                Text(
+                  'Football News',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                Padding(padding: EdgeInsets.all(10)),
+                Text(
+                  "Seluruh berita sepak bola terkini di sini!",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.white,
+                    fontWeight: FontWeight.normal, // weight biasa
+                  ),
+                ),
+              ],
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.home_outlined),
+            title: const Text('Home'),
+            // Bagian redirection ke MyHomePage
+            onTap: () {
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        MyHomePage(colorScheme: Theme.of(context).colorScheme),
+                  ));
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.post_add),
+            title: const Text('Add News'),
+            // redirection ke NewsFormPage
+            onTap: () {
+              // routing ke NewsFormPage
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const NewsFormPage(),
+                  ));
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
